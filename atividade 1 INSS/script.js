9ade8c44d340400e75cb8bab40637a408439c2c1
MellyssaS <160656359+MellyssaS@users.noreply.github.com>
2025-02-04 14:20:19 -0300

    Update script.js

--- atividade 1 INSS/script.js
+++ atividade 1 INSS/script.js
@@ -3,16 +3,16 @@ function calcularINSS() {
     let desconto = 0;
     let teto = 7087.22;
     
-    if (salario <= 1212.00) {
-        desconto = salario * 0.075;
-    }  if (salario <= 2427.35) {
-        desconto = salario * 0.09;
-    }  if (salario <= 3641.03) {
-        desconto = salario * 0.12;
-    }  if (salario <= teto) {
-        desconto = salario * 0.14;
+    if (salario <= 1212) {
+        desconto = salario * 7.5 / 100;
+    } else if (salario <= 2427) {
+        desconto = salario * 9 / 100;
+    } else if (salario <= 3641) {
+        desconto = salario * 12 / 100;
+    } else if (salario <= teto) {
+        desconto = salario * 14 /100;
     } else {
-        desconto = teto * 0.14;
+        desconto = teto * 14 /100;
     }
     
     let salarioFinal = salario - desconto;
