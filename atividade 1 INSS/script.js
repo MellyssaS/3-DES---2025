96bc314bad4571793102cc65f0e08d358eb17bd3
MellyssaS <160656359+MellyssaS@users.noreply.github.com>
2025-02-04 20:14:12 -0300

    Add files via upload

--- atividade 1 INSS/script.js
+++ atividade 1 INSS/script.js
@@ -1,20 +1,26 @@
-function calcularINSS() {
-    let salario = Number(document.getElementById("salario").value);
-    let desconto = 0;
-    let teto = 7087.22;
+function calcularMedia() {
+    let nota1 = parseFloat(document.getElementById("nota1").value);
+    let nota2 = parseFloat(document.getElementById("nota2").value);
+    let nota3 = parseFloat(document.getElementById("nota3").value);
     
-    if (salario <= 1212) {
-        desconto = salario * 7.5 / 100;
-    } else if (salario <= 2427) {
-        desconto = salario * 9 / 100;
-    } else if (salario <= 3641) {
-        desconto = salario * 12 / 100;
-    } else if (salario <= teto) {
-        desconto = salario * 14 /100;
-    } else {
-        desconto = teto * 14 /100;
+    let resultado = document.getElementById("resultado");
+
+    if (isNaN(nota1) || isNaN(nota2) || isNaN(nota3)) {
+        resultado.textContent = "Por favor, insira todas as notas corretamente!";
+        resultado.style.color = "red";
+        return;
     }
-    
-    let salarioFinal = salario - desconto;
-    document.getElementById("resultado").innerHTML = `Desconto do INSS: R$ ${desconto.toFixed(2)}<br>Salário líquido: R$ ${salarioFinal.toFixed(2)}`;
+
+    let media = (nota1 + nota2 + nota3) / 3;
+
+    if (media >= 6) {
+        resultado.textContent = `Média: ${media.toFixed(1)} - Aprovado`;
+        resultado.style.color = "green";
+    } else if (media >= 4) {
+        resultado.textContent = `Média: ${media.toFixed(1)} - Recuperação`;
+        resultado.style.color = "orange";
+    } else {
+        resultado.textContent = `Média: ${media.toFixed(1)} - Reprovado`;
+        resultado.style.color = "red";
+    }
 }
